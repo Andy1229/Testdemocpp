d2ebfad11e6a6251db2f50d41dfebfa9b8da1f3d
Andy <59327994+Andy1229@users.noreply.github.com>
2021-10-31 20:17:11 +0900

    Update Quadratic Equation.cpp

--- Quadratic Equation.cpp
+++ Quadratic Equation.cpp
@@ -1,3 +1,13 @@
+/*
+Implement the function findRoots to find the roots of the quadratic equation: ax2 + bx + c = 0. 
+If the equation has only one solution, the function should return that solution as both elements of the pair. 
+The equation will always have at least one solution.
+
+The roots of the quadratic equation can be found with the following formula: A quadratic equation.
+
+For example, the roots of the equation 2x2 + 10x + 8 = 0 are -1 and -4.
+*/
+
 #include <tuple>
 #include <stdexcept>
 #include <iostream>
